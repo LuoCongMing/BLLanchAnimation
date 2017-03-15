49360977054e4b494a061d860c036290491ea661
周建波 <zhoujianbo@zhoujianbodeMacBook-Pro.local>
2017-03-15 10:04:26 +0800

    Initial Commit

---
+++ LanchAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LanchAnimation
+//
+//  Created by 周建波 on 2017/3/15.
+//  Copyright © 2017年 scqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LanchAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LanchAnimation
+//
+//  Created by 周建波 on 2017/3/15.
+//  Copyright © 2017年 scqc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
